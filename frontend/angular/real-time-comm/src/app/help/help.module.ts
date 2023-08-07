79dd2c5d036e9511dd51de7eeb3add2e3320c50c
eddy08 <bhardwajharsh08@gmail.com>
2023-08-08 00:48:06 +0530

    Updated with Help Component and deleted Error Component

---
+++ frontend/angular/real-time-comm/src/app/help/help.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HelpComponent } from './help.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const routing = RouterModule.forChild([
+  { path: 'help-page', component: HelpComponent },
+  { path: '**', redirectTo: 'help-page' },
+]);
+@NgModule({
+  imports: [CommonModule, routing],
+  declarations: [HelpComponent],
+})
+export class HelpModule {}
